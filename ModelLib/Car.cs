a3adacb24cf69d390658dbdd17797c11733f25aa
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-09-30 15:33:27 +0200

    Add project files.

---
+++ ModelLib/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Type { get; set; }
+    }
+}
