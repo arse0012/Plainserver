a5a4036605d8064e7f290f35cc19c49c431bdf86
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-11-30 12:28:18 +0100

    JsonClient and JsonServer

---
+++ JsonServer/Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace JsonServer
+{
+    public class Server
+    {
+        public void Start()
+        {
+            TcpListener carServer = new TcpListener(IPAddress.Loopback, 10002);
+        }
+    }
+}
